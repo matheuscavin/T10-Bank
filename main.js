d6d54d471377d5c42a10a6160b95a7afa42e0bff
Matheus Cavinato Gomes <matheus.cavinato.gomes@gmail.com>
2021-01-22 01:16:50 -0300

    First Commit

--- main.js
+++ main.js
@@ -14,8 +14,19 @@ $(document).ready(function(){
         } else if(event.target.innerHTML === 'Contato') {
             section = 'contact';
         }
+
         $('html, body').animate({
             scrollTop: document.getElementById(section).offsetTop
         }, 800);
     });
 });
+
+function openMenu() {
+    let menu = document.getElementById('overflow')
+    console.log(menu.style.display)
+    if(menu.style.display !== 'block') {
+        menu.style.display = 'block';
+    } else {
+        menu.style.display = 'none';
+    }
+}
